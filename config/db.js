2a1039e00b35b2b745682663d9c5fadbb7a9b208
DLee1993 <Dai180293@gmail.com>
2022-11-15 03:29:17 +0000

    connected mongo database

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log("Database Connected");
+    } catch (error) {
+        console.error(error);
+        console.log("Database not connected");
+        //info - if error occurs the below code will exit the process
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const Express = require("express");
-
+const connectDB = require('./config/db'); 
 const PORT = process.env.PORT || 5000;
 const app = Express();
+
+//info - connect to the database
+connectDB(); 
 
 app.get("/", (req, res) => {
     res.send("api running");
