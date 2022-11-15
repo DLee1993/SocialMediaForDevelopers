f340db4eb7222b5bae715f36016be1b07be04b17
DLee1993 <Dai180293@gmail.com>
2022-11-15 06:33:43 +0000

    added validation to Register a new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //info - Route - api/users
-//info - Request type - GET
-//info - Desc - Test Route
+//info - Request type - POST
+//info - Desc - Register a new user
 //info - Access type - Public
-router.get("/", (req, res) => {
-    res.send("user route");
-});
+router.post(
+    "/",
+    [
+        check("name", "Please enter a name").not().isEmpty(),
+        check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password ( min length of 6 )").isLength({ min: 6 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+        res.send('User added')
+    }
+);
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const Express = require("express");
-const connectDB = require('./config/db'); 
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 const app = Express();
 
 //info - connect to the database
-connectDB(); 
+connectDB();
+
+//info - initialise middleware
+app.use(Express.json({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send("api running");
 });
 
 //info - Define Routes
-app.use('/users', require('./routes/api/users'))
-app.use('/auth', require('./routes/api/auth'))
-app.use('/profile', require('./routes/api/profile'))
-app.use('/posts', require('./routes/api/posts'))
+app.use("/users", require("./routes/api/users"));
+app.use("/auth", require("./routes/api/auth"));
+app.use("/profile", require("./routes/api/profile"));
+app.use("/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
