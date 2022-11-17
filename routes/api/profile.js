d9942377288b68eb5224342b557ce46fd2931e5b
DLee1993 <Dai180293@gmail.com>
2022-11-17 16:28:21 +0000

    connected the profile/me route to check if there is a user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Auth = require("../../middleware/auth");
+const userProfile = require("../../models/Profile");
+const User = require("../../models/User");
 
-//info - Route - api/profile
+//info - Route - /profile/me
 //info - Request type - GET
-//info - Desc - Test Route
-//info - Access type - Public
-router.get("/", (req, res) => {
-    res.send("profile route");
+//info - Desc - Get current user profile
+//info - Access type - Private - Token required
+router.get("/me", Auth, async (req, res) => {
+    try {
+        const profile = await userProfile
+            .findOne({ user: req.user.id })
+            .populate("user", ["name", "avatar"]);
+        if(!profile) return res.status(400).json({msg: 'There is no profile for this user'}); 
+        res.json(profile)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
 });
 
-module.exports = router; 
+module.exports = router;
