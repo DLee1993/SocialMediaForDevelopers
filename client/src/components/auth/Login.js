fc73635a96f69edea301e4e618a85b7ca899bbad
DLee1993 <Dai180293@gmail.com>
2022-11-20 17:00:53 +0000

    added the form and register jsx and the onChange function for state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div className='container'>Login</div>
-  )
-}
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const { email, password } = formData;
+
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('successful login')
+    };
+
+    return (
+        <div className='container'>
+            <h1 className='large text-primary'>Sign In</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Sign into your account
+            </p>
+            <form className='form' onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        minLength='6'
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                    />
+                </div>
+                <input type='submit' className='btn btn-primary' value='Login' />
+            </form>
+            <p className='my-1'>
+                Don't have an account? <Link to='/register'>Sign Up</Link>
+            </p>
+        </div>
+    );
+};
 
 export default Login
