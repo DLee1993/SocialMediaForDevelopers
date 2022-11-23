75c2ecc5cd8d4c0e412bcfb3ec01a3f90c051a24
DLee1993 <Dai180293@gmail.com>
2022-11-23 03:29:01 +0000

    created privateRoute for protected pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import UserAlert from "./components/layout/userAlert";
+import Dashboard from "./components/dashboard/Dashboard";
 // redux imports
 import { Provider } from "react-redux";
 import setAuthToken from "./utils/setAuthToken";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import "./App.css";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 if (localStorage.token) {
     setAuthToken(localStorage.token);
@@ -31,6 +33,7 @@ const App = () => {
                         <Route exact path='/' element={<Landing />} />
                         <Route exact path='/register' element={<Register />} />
                         <Route exact path='/login' element={<Login />} />
+                        <Route path='/dashboard' element={<PrivateRoute component={Dashboard} />} />
                     </Routes>
                 </Fragment>
             </BrowserRouter>
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const PrivateRoute = ({ component: Component, auth: { isAuthenticated } }) => {
+    if (isAuthenticated) return <Component />;
+
+    return <Navigate to='/login' />;
+};
+
+PrivateRoute.propTypes = {
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({auth: state.authReducer});
+
+export default connect(mapStateToProps)(PrivateRoute);
