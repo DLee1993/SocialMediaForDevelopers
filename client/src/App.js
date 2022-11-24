95209cbe639be660a4c23115410505127db67d6d
DLee1993 <Dai180293@gmail.com>
2022-11-24 20:14:40 +0000

    added the request to get all the developers who users of the site

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 // redux imports
 import { Provider } from "react-redux";
 import setAuthToken from "./utils/setAuthToken";
@@ -37,6 +38,7 @@ const App = () => {
                         <Route exact path='/' element={<Landing />} />
                         <Route exact path='/register' element={<Register />} />
                         <Route exact path='/login' element={<Login />} />
+                        <Route path='/profiles' element={<Profiles />}/>
                         <Route path='/dashboard' element={<PrivateRoute component={Dashboard} />} />
                         <Route
                             path='/create-profile'
